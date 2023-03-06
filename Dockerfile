96c8140f3568b19cfe468cfb8a90fe80b0c161fe
Your Name <you@example.com>
2023-03-06 10:55:46 -0500

    refactoring

--- Dockerfile
+++ Dockerfile
@@ -10,23 +10,17 @@ RUN apt update -y && apt upgrade -y && apt-get autoremove -y && apt-get clean -y
     pass \
     python3-pip \
     openssh-client \
-    jq \
-    python-tk \
     git \
     wget
 
 RUN pip3 install --no-cache-dir \
-    pipenv \
-    xmltodict \
-    PyJSONViewer \
-    requests \
-    pandas \
-    python-logging-loki
+    pipenv 
+
 
 
 WORKDIR /
 RUN mkdir APP RESULTS
 WORKDIR /APP
-COPY entrypoint.sh parse_zap.py config.py ./
+COPY entrypoint.sh ./
 RUN chmod +x entrypoint.sh && echo "export PATH=$PATH:/APP" >> /root/.bashrc
 ENTRYPOINT [ "bash", "entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 export PATH=$PATH:/APP
 if [ "$1" = "zap_vuln_scan" ]; then
-    printf "passing args to zap: %s %s %s %s" "$2" "$3" "$4" "$5"
+    printf "passing args to zap: %s" "$2" 
     owasp-zap -cmd -quickurl "$2" -quickout /RESULTS/zap_raw_results.json -silent -quickprogress &&
         jq . /RESULTS/zap_raw_results.json >/RESULTS/zap_processed_results_.json
+
+elif [ "$1" = "nmap_vuln_scan" ]; then
+    printf "passing args to nmap: %s" "$2" 
+    nmap -sV --script nmap-vulners/ "$2" > /RESULTS/nmap_raw_results
+
+elif [ "$1" = "nmap_vuln_scan_list" ]; then
+    while read -r ip
+    do
+        nmap -sV --script nmap-vulners/ "$ip" >> /RESULTS/nmap_raw_results
+    done < ips.txt
 
 else
     printf "unrecognized command"
