306e05573ad27604fac1ebe94ad89267b7f91e8d
dige-mothership <dige@dige.tech>
2023-05-27 18:28:13 -0400

    testing

--- scripts/bash/check_if_smb_signing_is_required.sh
+++ scripts/bash/check_if_smb_signing_is_required.sh
@@ -2,7 +2,7 @@
 
 # if it is a file that contains a list of targets then use crackmap
 if [ -f "$1" ]; then
-    crackmapexec smb targets "$1"
+    crackmapexec smb targets "$1" > /RESULTS/results
 else
-    crackmapexec smb "$1"
+    crackmapexec smb "$1" > /RESULTS/results
 fi
