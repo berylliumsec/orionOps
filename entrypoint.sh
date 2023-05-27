5334ff2d5dfb729fc07efe172a9174ab8795e3be
dige-mothership <dige@dige.tech>
2023-05-27 18:57:12 -0400

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,7 +46,7 @@ if [ "$1" = "zap_vuln_scan" ]; then
     fi
     
     elif [ "$1" = "check_if_smb_signing_is_required" ]; then
-    /scripts/bash/check_if_smb_signing_is_required.sh "$2"
+    /scripts/bash/check_if_smb_signing_is_required.sh "$2" 1>&0
     
     elif [ "$1" = "check_for_ipv6_traffic" ]; then
     /scripts/bash/check_for_ipv6_traffic.sh "$2"
