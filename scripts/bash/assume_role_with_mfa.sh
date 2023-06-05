574dc5cda7fc88a65fa696c0ecca94eb0952b549
David I <MY_NAME@example.com>
2023-06-05 10:42:39 -0400

    adding some debugging

--- scripts/bash/assume_role_with_mfa.sh
+++ scripts/bash/assume_role_with_mfa.sh
@@ -3,6 +3,10 @@ response="$(aws sts assume-role --no-paginate --role-arn "$1" --role-session-nam
 SessionToken=$(jq -r '.Credentials.SessionToken' <<< "$response")
 AccessKeyId=$(jq -r '.Credentials.AccessKeyId' <<< "$response")
 SecretAccessKey=$(jq -r '.Credentials.SecretAccessKey' <<< "$response")
+echo "Session token: $SessionToken"
+echo "Access Key Id: $AccessKeyId"
+echo "Secret Access Key: $SecretAccessKey"
 export AWS_ACCESS_KEY_ID="$SecretAccessKey"
 export AWS_SECRET_ACCESS_KEY="$SessionToken"
 export AWS_SESSION_TOKEN="$AccessKeyId"
+env
