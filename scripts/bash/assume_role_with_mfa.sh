3922bb9a5c2aa689de1d4cf8959086012b31aa4b
David I <MY_NAME@example.com>
2023-06-05 11:05:37 -0400

    fixing errors

--- scripts/bash/assume_role_with_mfa.sh
+++ scripts/bash/assume_role_with_mfa.sh
@@ -3,10 +3,10 @@ response="$(aws sts assume-role --no-paginate --role-arn "$1" --role-session-nam
 SessionToken=$(jq -r '.Credentials.SessionToken' <<< "$response")
 AccessKeyId=$(jq -r '.Credentials.AccessKeyId' <<< "$response")
 SecretAccessKey=$(jq -r '.Credentials.SecretAccessKey' <<< "$response")
-echo "Session token: $SessionToken"
-echo "Access Key Id: $AccessKeyId"
-echo "Secret Access Key: $SecretAccessKey"
-export AWS_ACCESS_KEY_ID="$SecretAccessKey"
-export AWS_SECRET_ACCESS_KEY="$AccessKeyId"
+echo "export AWS_ACCESS_KEY_ID=$AccessKeyId"
+echo "export AWS_SECRET_ACCESS_KEY=$SecretAccessKey"
+echo "AWS_SESSION_TOKEN=$SessionToken"
+export AWS_ACCESS_KEY_ID="$AccessKeyId"
+export AWS_SECRET_ACCESS_KEY="$SecretAccessKey"
 export AWS_SESSION_TOKEN="$SessionToken"
 
