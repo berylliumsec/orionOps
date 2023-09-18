a33fbff0e18fee23d249750ad2270e4f0ffcb4a1
David I <david@berylliumsec.com>
2023-09-18 16:52:40 -0400

    updating powershell

--- scripts/powershell/enumerate_acess.ps1
+++ scripts/powershell/enumerate_acess.ps1
@@ -18,10 +18,22 @@ if (-not $aclObject) {
 
 # Print the access rules
 foreach ($rule in $aclObject.Access) {
-    Write-Host "IdentityReference: $($rule.IdentityReference)"
+    # Try to resolve the SID to a friendly name
+    try {
+        $resolvedIdentity = $rule.IdentityReference.Translate([System.Security.Principal.NTAccount])
+    } catch {
+        # If translation fails, use the original SID as the resolved identity
+        $resolvedIdentity = $rule.IdentityReference.ToString()
+    }
+
+    Write-Host "IdentityReference (SID): $($rule.IdentityReference)"
+    Write-Host "IdentityReference (Name): $resolvedIdentity"
     Write-Host "AccessControlType: $($rule.AccessControlType)"
     Write-Host "Rights: $($rule.ActiveDirectoryRights)"
     Write-Host "InheritanceType: $($rule.InheritanceType)"
+    Write-Host "IsInherited: $($rule.IsInherited)"  # Indicates if the rule is inherited from a parent object
+    Write-Host "InheritanceFlags: $($rule.InheritanceFlags)"  # Provides more granularity on inheritance type
+    Write-Host "PropagationFlags: $($rule.PropagationFlags)"  # Specifies how inheritance is propagated
     Write-Host "ObjectType: $($rule.ObjectType)"
     Write-Host "--------------------------------------------"
 }
