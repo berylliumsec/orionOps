fc211d69d59303cceb869e624cdeafc4df7a4e4b
David I <david@berylliumsec.com>
2023-09-28 07:04:36 -0400

    fixing target list issue

--- scripts/bash/check_if_smb_signing_is_required.sh
+++ scripts/bash/check_if_smb_signing_is_required.sh
@@ -2,7 +2,7 @@
 
 # if it is a file that contains a list of targets then use crackmap
 if [ -f "/RESULTS/$1" ]; then
-    crackmapexec smb target "$1"
+    crackmapexec smb target "/RESULTS/$1"
 else
     crackmapexec smb "$1"
 fi
